db1877b98a924d9736f15cd16ba48d968d7acb91
Floris Doolaard <floris.do@gmail.com>
2016-05-09 11:10:34 +0200

    Implented example RouteNodes.
    Added different places to go to in a Route.

---
+++ src/main/java/pixelperfect/route/MarsNode.java
@@ -0,0 +1,23 @@
+package pixelperfect.route;
+
+/**
+ * A planet Node called Mars which belongs in a Route.
+ * 
+ * @author Floris Doolaard
+ *
+ */
+public class MarsNode extends RouteNode {
+
+  /**
+   * The constructor for the MarsNode.
+   * 
+   * @param summary
+   *          , a summary of the MarsNode.
+   * @param description
+   *          , a description of the MarsNode.
+   */
+  public MarsNode(String summary, String description) {
+    super(summary, description);
+  }
+
+}
--- src/main/java/pixelperfect/route/RouteNode.java
+++ src/main/java/pixelperfect/route/RouteNode.java
@@ -4,8 +4,24 @@ package pixelperfect.route;
  * Node along a route through space.
  * 
  * @author Jesse Tilro
+ * @author Floris Doolaard
  *
  */
 public class RouteNode {
 
+  private String summary;
+  private String description;
+
+  /**
+   * A constructor for the RouteNode.
+   * 
+   * @param summary
+   *          , a summary about the RouteNode.
+   * @param description
+   *          , a description about the RouteNode.
+   */
+  public RouteNode(String summary, String description) {
+    this.summary = summary;
+    this.description = description;
+  }
 }
