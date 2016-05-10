2ed53aff5b6ed975fb72fa59ede65c27bb0ab238
Wouter Zirkzee <Wouter Zirkzee>
2016-05-10 20:26:28 +0200

    Fix of small error and changed event descriptions

--- src/main/java/nl/tudelft/pixelperfect/event/EventFactory.java
+++ src/main/java/nl/tudelft/pixelperfect/event/EventFactory.java
@@ -26,7 +26,7 @@ public class EventFactory {
    * @return an Event.
    */
   public Event randomEvent() {
-    int x = (int) Math.floor(Math.random()) % 4;
+    int x = (int) Math.floor(Math.random() * 100) % 4;
     return create(x);
   }
 
@@ -40,16 +40,16 @@ public class EventFactory {
     Event tbr = null;
     switch (x) {
       case 0:
-        tbr = new AsteroidField(id, "Asteroid", "Hello world, I am a dummy Asteroid event!", System.currentTimeMillis(), 4000, 10);
+        tbr = new AsteroidField(id, "Asteroid Field", "Watch out, you are approaching an asteroid field!", System.currentTimeMillis(), 4000, 10);
         break;
       case 1:
-        tbr = new Fire(id, "Fire", "Hello world, I am a dummy Fire event!", System.currentTimeMillis(), 4000, 10);
+        tbr = new Fire(id, "Fire", "Alert! Faulty wiring caused a fire!", System.currentTimeMillis(), 4000, 10);
         break;
       case 2:
-        tbr = new HostileShip(id, "HostileShip", "Hello world, I am a dummy HostileShip event!", System.currentTimeMillis(), 4000, 10);
+        tbr = new HostileShip(id, "Hostile Ship", "A hostile spaceship is near, prepare to defend yourself!", System.currentTimeMillis(), 4000, 10);
         break;
       case 3:
-        tbr = new PlasmaLeak(id, "PlasmaLeak", "Hello world, I am a dummy PlasmaLeak event!", System.currentTimeMillis(), 4000, 10);
+        tbr = new PlasmaLeak(id, "Plasma Leak", "Plasma pressure is dropping, there must be a leak!", System.currentTimeMillis(), 4000, 10);
         break;
     }
     id++;
