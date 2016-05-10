fca0de2aba255bbefe935c709677d1be06f21a9f
David Alderliesten <david.j.w@hotmail.com>
2016-05-10 16:20:42 +0200

    Create route test
    Created the tests for the entire route node suite, based off of the test
    develope by Floris and Jesse.

--- src/test/java/nl/tudelft/pixelperfect/route/MarsNodeTest.java
+++ src/test/java/nl/tudelft/pixelperfect/route/MarsNodeTest.java
@@ -1,11 +1,40 @@
 package nl.tudelft.pixelperfect.route;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Test Suite for the MarsNode class.
  * 
- * @author Floris Doolaard
+ * @author David Alderliesten
  *
  */
 public class MarsNodeTest extends RouteNodeTest {
+  private MarsNode node;
 
+  /**
+   * Initializing objects.
+   */
+  @Before
+  public void before() {
+    node = new MarsNode("summary", "description");
+  }
+
+  /**
+   * Testing the getSummary method.
+   */
+  @Test
+  public void testRouteSummary() {
+    assertEquals("summary", node.getSummary());
+  }
+
+  /**
+   * Testing the getDescription method.
+   */
+  @Test
+  public void testRouteDescription() {
+    assertEquals("description", node.getDescription());
+  }
 }
