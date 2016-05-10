4360ff4fd34079582b3ceddbb793e0e3d16b2981
David Alderliesten <david.j.w@hotmail.com>
2016-05-10 15:23:46 +0200

    Hotfix playertest
    Hotfixed the player test classes to correctly utilize their dependency
    class.

--- src/test/java/nl/tudelft/pixelperfect/player/PlayerTest.java
+++ src/test/java/nl/tudelft/pixelperfect/player/PlayerTest.java
@@ -1,33 +1,11 @@
 package nl.tudelft.pixelperfect.player;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Before;
-import org.junit.Test;
-
 /**
- * Test Suite for the Player class.
+ * Class for the automated testing of the Player class.
  * 
  * @author David Alderliesten
- * @author Jesse Tilro
  *
  */
-public abstract class PlayerTest {
-  private Player toTest;
+public class PlayerTest {
 
-  /**
-   * Setting up the Event class for the test.
-   */
-  @Before
-  public void initalise() {
-    toTest = new Player("Lorem Ipsum");
-  }
-
-  /**
-   * Verify the behavior of the getName method.
-   */
-  @Test
-  public void testGetName() {
-    assertEquals(toTest.getName(), "Lorem Ipsum");
-  }
 }
