e65e7a1a40ff77d35430709ce134b971e91fa4ae
Wouter Zirkzee <Wouter Zirkzee>
2016-05-10 14:15:43 +0200

    Refactored testing with parallel testing hierarcy.

--- src/test/java/nl/tudelft/pixelperfect/event/EventTest.java
+++ src/test/java/nl/tudelft/pixelperfect/event/EventTest.java
@@ -13,66 +13,67 @@ import org.junit.Test;
  * @author David Alderliesten
  *
  */
-public class EventTest {
+public abstract class EventTest {
 
-//  private Event toTest;
-//
-//  /**
-//   * Setting up the Event class for the test.
-//   */
-//  @Before
-//  public void initialize() {
-//    toTest = new Event(1, "TestEvent", "An Event to test the Class.", 42, 42, 99.42) {
-//    };
-//  }
-//
-//  /**
-//   * Testing the getId method.
-//   */
-//  @Test
-//  public void testGetId() {
-//    assertEquals(toTest.getId(), 1);
-//  }
-//
-//  /**
-//   * Testing the getSummary method.
-//   */
-//  @Test
-//  public void testGetSummary() {
-//    assertEquals(toTest.getSummary(), "TestEvent");
-//  }
-//
-//  /**
-//   * Testing the getDescription method.
-//   */
-//  @Test
-//  public void testGetDescription() {
-//    assertEquals(toTest.getDescription(), "An Event to test the Class.");
-//  }
-//
-//  /**
-//   * When the duration of an event has not yet passed since it's creation, it should not be
-//   * recognized as expired.
-//   */
-//  @Test
-//  public void testIsExpiredFalse() {
-//    assertFalse(toTest.isExpired(83));
-//  }
-//
-//  /**
-//   * When the duration of an event has passed since it's creation, it should be recognized as
-//   * expired.
-//   */
-//  @Test
-//  public void testIsExpiredTrue() {
-//    assertTrue(toTest.isExpired(85));
-//  }
-//
-//  /**
-//   * Testing the getDamage method.
-//   */
-//  @Test
-//  public void testGetDamage() {
-//    assertEquals(toTest.getDamage(), 99.42, 0);
-//  }
+  private Event toTest;
+
+  /**
+   * Setting up the Event class for the test.
+   */
+  @Before
+  public void initialize() {
+    toTest = createEvent();
+  }
+
+  public abstract Event createEvent();
+
+  /**
+   * Testing the getId method.
+   */
+  @Test
+  public void testGetId() {
+    assertEquals(toTest.getId(), 1);
+  }
+
+  /**
+   * Testing the getSummary method.
+   */
+  @Test
+  public void testGetSummary() {
+    assertEquals(toTest.getSummary(), "TestEvent");
+  }
+
+  /**
+   * Testing the getDescription method.
+   */
+  @Test
+  public void testGetDescription() {
+    assertEquals(toTest.getDescription(), "An Event to test the Class.");
+  }
+
+  /**
+   * When the duration of an event has not yet passed since it's creation, it should not be
+   * recognized as expired.
+   */
+  @Test
+  public void testIsExpiredFalse() {
+    assertFalse(toTest.isExpired(83));
+  }
+
+  /**
+   * When the duration of an event has passed since it's creation, it should be recognized as
+   * expired.
+   */
+  @Test
+  public void testIsExpiredTrue() {
+    assertTrue(toTest.isExpired(85));
+  }
+
+  /**
+   * Testing the getDamage method.
+   */
+  @Test
+  public void testGetDamage() {
+    assertEquals(toTest.getDamage(), 99.42, 0);
+  }
 }
--- src/test/java/nl/tudelft/pixelperfect/event/FireTest.java
+++ src/test/java/nl/tudelft/pixelperfect/event/FireTest.java
@@ -8,66 +8,11 @@ import static org.junit.Assert.*;
 /**
  * Created by woute on 5/10/2016.
  */
-public class FireTest  {
+public class FireTest extends EventTest{
 
-    private Fire toTest;
-
-    /**
-     * Setting up the Fire class for the test.
-     */
-    @Before
-    public void initialize() {
-        toTest = new Fire(1, "TestEvent", "An Event to test the Class.", 42, 42, 99.42);
-    }
-
-    /**
-     * Testing the getId method.
-     */
-    @Test
-    public void testGetId() {
-        assertEquals(toTest.getId(), 1);
-    }
-
-    /**
-     * Testing the getSummary method.
-     */
-    @Test
-    public void testGetSummary() {
-        assertEquals(toTest.getSummary(), "TestEvent");
-    }
-
-    /**
-     * Testing the getDescription method.
-     */
-    @Test
-    public void testGetDescription() {
-        assertEquals(toTest.getDescription(), "An Event to test the Class.");
-    }
-
-    /**
-     * When the duration of an event has not yet passed since it's creation, it should not be
-     * recognized as expired.
-     */
-    @Test
-    public void testIsExpiredFalse() {
-        assertFalse(toTest.isExpired(83));
-    }
-
-    /**
-     * When the duration of an event has passed since it's creation, it should be recognized as
-     * expired.
-     */
-    @Test
-    public void testIsExpiredTrue() {
-        assertTrue(toTest.isExpired(85));
-    }
-
-    /**
-     * Testing the getDamage method.
-     */
-    @Test
-    public void testGetDamage() {
-        assertEquals(toTest.getDamage(), 99.42, 0);
+    @Override
+    public Fire createEvent() {
+        return new Fire(1, "TestEvent", "An Event to test the Class.", 42, 42, 99.42);
     }
 
 }
