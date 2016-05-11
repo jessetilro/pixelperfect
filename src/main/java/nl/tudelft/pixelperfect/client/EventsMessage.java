2db847094e1201facef8abdcd712823509e498bf
Dmitry Malarev <D.R.Malarev@student.tudelft.nl>
2016-05-11 20:52:37 +0200

    Merged with new release

---
+++ src/main/java/nl/tudelft/pixelperfect/client/EventsMessage.java
@@ -0,0 +1,47 @@
+package nl.tudelft.pixelperfect.client;
+
+import java.util.ArrayList;
+
+import com.jme3.network.AbstractMessage;
+import com.jme3.network.serializing.Serializable;
+
+import nl.tudelft.pixelperfect.event.Event;
+
+/**
+ * Massage type that sends the event log.
+ * 
+ * @author Dmitry
+ *
+ */
+@Serializable
+public class EventsMessage extends AbstractMessage {
+  
+  private ArrayList<Event> eve;
+
+  /**
+   * The constructor of the events message.
+   * 
+   */
+  public EventsMessage() {
+  }
+
+  /**
+   * Changes the log.
+   * 
+   * @param passedLog
+   *          the event list that will be the new log message.
+   */
+  public EventsMessage(ArrayList<Event> passedLog) {
+    eve = passedLog;
+  }
+
+  /**
+   * Returns the log.
+   * 
+   * @return the log.
+   */
+  public ArrayList<Event> getLog() {
+    return eve;
+  }
+
+}
