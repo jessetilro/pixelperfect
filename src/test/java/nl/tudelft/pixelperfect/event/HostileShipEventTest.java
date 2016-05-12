732397c65f72b3b9681753ba375116ecb072452d
Floris Doolaard <floris.do@gmail.com>
2016-05-12 21:09:04 +0200

    Fixed more Checkstyle warnings.
    Mainly formatting warnings.

--- src/test/java/nl/tudelft/pixelperfect/event/HostileShipEventTest.java
+++ src/test/java/nl/tudelft/pixelperfect/event/HostileShipEventTest.java
@@ -7,11 +7,12 @@ public class HostileShipEventTest extends EventTest {
 
   /**
    * Factory method for testing.
+   * 
    * @return PlasmaLeakEvent class
    */
   @Override
   public HostileShipEvent createEvent() {
-      return new HostileShipEvent(1, "TestEvent", "An Event to test the Class.", 42, 42, 99.42);
-    }
+    return new HostileShipEvent(1, "TestEvent", "An Event to test the Class.", 42, 42, 99.42);
+  }
 
 }
