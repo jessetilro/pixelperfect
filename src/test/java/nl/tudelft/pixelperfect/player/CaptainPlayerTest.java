ef87dab6d7dbb0e862f36bd29d5d7c6f5288e223
Jesse Tilro <jesse@jessetilro.nl>
2016-05-12 21:47:39 +0200

    Update Player test suites for parallel hierarchy.
    Updated the Player test suites to form a parallel class hierarchy.

--- src/test/java/nl/tudelft/pixelperfect/player/CaptainPlayerTest.java
+++ src/test/java/nl/tudelft/pixelperfect/player/CaptainPlayerTest.java
@@ -1,12 +1,5 @@
 package nl.tudelft.pixelperfect.player;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Before;
-import org.junit.Test;
-
 /**
  * Test Suite for the CaptainPlayer class.
  * 
@@ -15,34 +8,13 @@ import org.junit.Test;
  *
  */
 public class CaptainPlayerTest extends PlayerTest {
-  private CaptainPlayer toTest;
 
   /**
-   * Setting up the Event class for the test.
+   * Create a CaptainPlayer instance as test object.
    */
-  @Before
-  public void initalise() {
-    toTest = new CaptainPlayer("Lorem Ipsum");
-  }
-
-  /**
-   * Verify the behavior of the getName method, which is inherited from the player class..
-   */
-  @Test
-  public void testGetName() {
-    assertEquals(toTest.getName(), "Lorem Ipsum");
-  }
-
-  @Test
-  public void testEqualsTrue() {
-    CaptainPlayer otherTest = new CaptainPlayer("Lorem Ipsum");
-    assertTrue(toTest.equals(otherTest));
-  }
-
-  @Test
-  public void testEqualsFalse() {
-    CaptainPlayer otherTest = new CaptainPlayer("Foo Bar");
-    assertFalse(toTest.equals(otherTest));
+  @Override
+  public CaptainPlayer createPlayer(String name) {
+    return new CaptainPlayer(name);
   }
 
 }
--- src/test/java/nl/tudelft/pixelperfect/player/PlayerTest.java
+++ src/test/java/nl/tudelft/pixelperfect/player/PlayerTest.java
@@ -1,11 +1,61 @@
 package nl.tudelft.pixelperfect.player;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Class for the automated testing of the Player class.
  * 
  * @author David Alderliesten
  *
  */
-public class PlayerTest {
+public abstract class PlayerTest {
+
+  private Player testObject;
+
+  /**
+   * Setting up the Player class as a test object.
+   */
+  @Before
+  public void initialize() {
+    testObject = createPlayer("Lorem Ipsum");
+  }
+
+  /**
+   * Factory method for testing.
+   * 
+   * @return The object to be tested.
+   */
+  public abstract Player createPlayer(String name);
+
+  /**
+   * Verify the behavior of the getName method, which is inherited from the player class..
+   */
+  @Test
+  public void testGetName() {
+    assertEquals(testObject.getName(), "Lorem Ipsum");
+  }
+
+  /**
+   * Two players with the same name should be considered equal.
+   */
+  @Test
+  public void testEqualsTrue() {
+    Player otherTest = createPlayer("Lorem Ipsum");
+    assertTrue(testObject.equals(otherTest));
+  }
+
+  /**
+   * Two players with a different name should not be considered equal.
+   */
+  @Test
+  public void testEqualsFalse() {
+    Player otherTest = createPlayer("Foo Bar");
+    assertFalse(testObject.equals(otherTest));
+  }
 
 }
