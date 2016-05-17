a185a7d024714192eebb9f8cbcc9e58336dda066
wouterz <zirkzee@hotmail.com>
2016-05-17 17:46:17 +0200

    Able to import assets and changed variable names as there were "too short"
    Ability to import assets removes error drawTimer gave for not being able to import the font.  This did make starting up the game extremely slow for some reason.  Also added a background from example project just for the looks of it.

--- src/main/java/nl/tudelft/pixelperfect/Scene.java
+++ src/main/java/nl/tudelft/pixelperfect/Scene.java
@@ -9,7 +9,9 @@ import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
+import com.jme3.util.SkyFactory;
 
 /**
  * Class for drawing objects in the game.
@@ -39,6 +41,11 @@ public class Scene {
    * Method that contains all objects for the scene.
    */
   public void createMap() {
+    Spatial sky = SkyFactory.createSky(
+        app.getAssetManager(), "Textures/Sky/Bright/spheremap.png", 
+        SkyFactory.EnvMapType.EquirectMap);
+    app.getRootNode().attachChild(sky);
+  	
     drawDashboard();
     drawFloor();
     drawWalls();
@@ -51,12 +58,12 @@ public class Scene {
    */
   private void drawDashboard() {
     Box dashboard = new Box(8, 1f, 1);
-    Geometry geom = new Geometry("Box", dashboard);
-    Material mat = new Material(app.getAssetManager(), basicMat);
-    mat.setColor(color, ColorRGBA.Blue);
-    geom.setMaterial(mat);
-    geom.setLocalTranslation(new Vector3f(0, 0, 7));
-    app.getRootNode().attachChild(geom);
+    Geometry geometry = new Geometry("Box", dashboard);
+    Material material = new Material(app.getAssetManager(), basicMat);
+    material.setColor(color, ColorRGBA.Blue);
+    geometry.setMaterial(material);
+    geometry.setLocalTranslation(new Vector3f(0, 0, 7));
+    app.getRootNode().attachChild(geometry);
   }
 
   /**
@@ -65,12 +72,12 @@ public class Scene {
   private void drawFloor() {
     // green floor
     Box floor = new Box(8, 0.01f, 8);
-    Geometry g2 = new Geometry("Floor", floor);
-    Material mat2 = new Material(app.getAssetManager(), basicMat);
-    mat2.setColor(color, ColorRGBA.Green);
-    g2.setMaterial(mat2);
-    g2.setLocalTranslation(0, -1, 0);
-    app.getRootNode().attachChild(g2);
+    Geometry geometry = new Geometry("Floor", floor);
+    Material material = new Material(app.getAssetManager(), basicMat);
+    material.setColor(color, ColorRGBA.Green);
+    geometry.setMaterial(material);
+    geometry.setLocalTranslation(0, -1, 0);
+    app.getRootNode().attachChild(geometry);
     
   }
 
@@ -80,32 +87,32 @@ public class Scene {
   private void drawWalls() {
     // walls
     Box wallLeft = new Box(0.01f, 8, 8);
-    Geometry l1 = new Geometry("leftwall", wallLeft);
-    Material mat3 = new Material(app.getAssetManager(), basicMat);
-    mat3.setColor(color, ColorRGBA.Orange);
-    l1.setMaterial(mat3);
-    l1.setLocalTranslation(new Vector3f(-8, 7, 0));
-    app.getRootNode().attachChild(l1);
+    Geometry geometry1 = new Geometry("leftwall", wallLeft);
+    Material material1 = new Material(app.getAssetManager(), basicMat);
+    material1.setColor(color, ColorRGBA.Orange);
+    geometry1.setMaterial(material1);
+    geometry1.setLocalTranslation(new Vector3f(-8, 7, 0));
+    app.getRootNode().attachChild(geometry1);
 
     Box wallRight = new Box(0.01f, 8, 8);
-    Geometry l2 = new Geometry("rightwall", wallRight);
-    l2.setMaterial(mat3);
-    l2.setLocalTranslation(new Vector3f(8, 7, 0));
-    app.getRootNode().attachChild(l2);
+    Geometry geometry2 = new Geometry("rightwall", wallRight);
+    geometry2.setMaterial(material1);
+    geometry2.setLocalTranslation(new Vector3f(8, 7, 0));
+    app.getRootNode().attachChild(geometry2);
 
     Box wallFront = new Box(8, 8, 0.01f);
-    Geometry l3 = new Geometry("frontwall", wallFront);
-    Material mat4 = new Material(app.getAssetManager(), basicMat);
-    mat4.setColor(color, ColorRGBA.Yellow);
-    l3.setMaterial(mat4);
-    l3.setLocalTranslation(new Vector3f(0, 7, -8));
-    app.getRootNode().attachChild(l3);
+    Geometry geometry3 = new Geometry("frontwall", wallFront);
+    Material material2 = new Material(app.getAssetManager(), basicMat);
+    material2.setColor(color, ColorRGBA.Yellow);
+    geometry3.setMaterial(material2);
+    geometry3.setLocalTranslation(new Vector3f(0, 7, -8));
+    app.getRootNode().attachChild(geometry3);
 
     Box wallBack = new Box(8, 8, 0.01f);
-    Geometry l4 = new Geometry("backwall", wallBack);
-    l4.setMaterial(mat4);
-    l4.setLocalTranslation(new Vector3f(0, 7, 8));
-    app.getRootNode().attachChild(l4);
+    Geometry geometry4 = new Geometry("backwall", wallBack);
+    geometry4.setMaterial(material2);
+    geometry4.setLocalTranslation(new Vector3f(0, 7, 8));
+    app.getRootNode().attachChild(geometry4);
   }
 
   /**
@@ -134,20 +141,20 @@ public class Scene {
    * 						Amount of collums of buttons.
    */
   private void addButton(Vector3f location, int row, int col) {
-  	float dist = 1f;
+  	float distance = 1f;
   	for (int x = 0; x < row; x++) {
   		for (int z = 0; z < col; z++) {
   			Box box = new Box(1f, 1f, 1f);
-  	    Geometry but = new Geometry("Box", box);
-  	    but.setLocalScale(0.2f);
+  	    Geometry button = new Geometry("Box", box);
+  	    button.setLocalScale(0.2f);
   	  	Material buttonMat = new Material(app.getAssetManager(), basicMat);
   	  	buttonMat.setColor("Color", ColorRGBA.Red);
-  	    but.setMaterial(buttonMat);
-  	    but.setLocalTranslation(
-  	    		new Vector3f(location.getX() + x * dist,
-  	    				location.getY(), location.getZ() + z * dist));
-  	    app.getRootNode().attachChild(but);
-  	    buttons.add(but);
+  	    button.setMaterial(buttonMat);
+  	    button.setLocalTranslation(
+  	    		new Vector3f(location.getX() + x * distance,
+  	    				location.getY(), location.getZ() + z * distance));
+  	    app.getRootNode().attachChild(button);
+  	    buttons.add(button);
   		}
   	}
   }
