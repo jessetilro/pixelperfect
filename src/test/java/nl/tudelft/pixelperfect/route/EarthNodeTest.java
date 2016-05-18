3b4dbe884cfdeed3c0581b5a06823801e3f44e77
Jesse Tilro <jesse@jessetilro.nl>
2016-05-18 11:50:05 +0200

    Update RouteNode to be abstract.
    The RouteNode class should be abstract, since we should work with its
    subtypes. Updated the test suite hierarchy to be parallel, using the
    factory method design pattern.

--- src/test/java/nl/tudelft/pixelperfect/route/EarthNodeTest.java
+++ src/test/java/nl/tudelft/pixelperfect/route/EarthNodeTest.java
@@ -2,47 +2,32 @@ package nl.tudelft.pixelperfect.route;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Test Suite for the EarthNode class.
  * 
  * @author David Alderliesten
+ * @author Jesse Tilro
  *
  */
 public class EarthNodeTest extends RouteNodeTest {
-  private EarthNode node;
 
   /**
-   * Initializing objects.
+   * Create a test object.
+   * 
+   * @return A RouteNode.
    */
-  @Before
-  public void before() {
-    node = new EarthNode("summary", "description");
+  @Override
+  protected RouteNode create() {
+    return new EarthNode("summary", "description");
   }
 
-  /**
-   * Testing the getSummary method.
-   */
-  @Test
-  public void testRouteSummary() {
-    assertEquals("summary", node.getSummary());
-  }
-
-  /**
-   * Testing the getDescription method.
-   */
-  @Test
-  public void testRouteDescription() {
-    assertEquals("description", node.getDescription());
-  }
-  
   /**
    * Testing toString.
    */
   @Test
   public void testToString() {
-    assertEquals("EarthNode", node.toString());
+    assertEquals("EarthNode", getObject().toString());
   }
 }
