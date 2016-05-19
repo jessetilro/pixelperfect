7333887c212355ac7a35228f9d05c748680abd5c
David Alderliesten <david.j.w@hotmail.com>
2016-05-19 19:19:07 +0200

    Create constants
    Created the constants class.

---
+++ src/main/java/nl/tudelft/pixelperfect/Constants.java
@@ -0,0 +1,11 @@
+package nl.tudelft.pixelperfect;
+
+/**
+ * A data class for storing game-wide items that can all be altered in a single sweep.
+ * 
+ * @author David Alderliesten
+ *
+ */
+public final class Constants {
+
+}
