c223919d9f353770e21ad5c292dcf926c8729576
David Alderliesten <david.j.w@hotmail.com>
2016-05-19 19:10:58 +0200

    Establish base
    Created a base class for the heads-up display.

---
+++ src/main/java/nl/tudelft/pixelperfect/gui/GameHeadsUpDisplay.java
@@ -0,0 +1,11 @@
+package nl.tudelft.pixelperfect.gui;
+
+/**
+ * Class that displays relevant information in the heads-up display during the gameplay.
+ * 
+ * @author David Alderliesten
+ *
+ */
+public class GameHeadsUpDisplay {
+
+}
