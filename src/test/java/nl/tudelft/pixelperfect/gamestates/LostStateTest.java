d2dcdcbebd619b19e41be723fc8b6f12088205ea
wouterz <zirkzee@hotmail.com>
2016-05-24 19:29:17 +0200

    empty test classes

---
+++ src/test/java/nl/tudelft/pixelperfect/gamestates/LostStateTest.java
@@ -0,0 +1,27 @@
+package nl.tudelft.pixelperfect.gamestates;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by woute on 5/24/2016.
+ */
+public class LostStateTest extends GameStateTest {
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @Test
+  public void update() throws Exception {
+
+  }
+
+  @Test
+  public void handleState() throws Exception {
+
+  }
+
+}
