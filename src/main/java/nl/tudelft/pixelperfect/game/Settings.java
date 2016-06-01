5486120096faade56d131e5cda9a8c0d72c2ec40
David Alderliesten <david.j.w@hotmail.com>
2016-06-01 13:31:14 +0200

    Finalize Settings
    Finalized the initial settings class.

--- src/main/java/nl/tudelft/pixelperfect/game/Settings.java
+++ src/main/java/nl/tudelft/pixelperfect/game/Settings.java
@@ -1,11 +1,50 @@
 package nl.tudelft.pixelperfect.game;
 
 /**
- * A singleton data class for settings tracking that are used accross the game.
+ * A singleton data class for settings tracking that are used across the game.
  * 
  * @author David Alderliesten
  *
  */
 public final class Settings {
+  private static volatile Settings instance;
+
+  private static boolean isDebug = false;
+
+  /**
+   * Gets the current instance of the settings class.
+   * 
+   * @return the settings instance
+   */
+  public static Settings getInstance() {
+    if (instance == null) {
+      synchronized (Settings.class) {
+        instance = new Settings();
+      }
+    }
+    return instance;
+  }
+
+  /**
+   * Constructor for the Settings class.
+   */
+  private Settings() {
+  }
+
+  /**
+   * Get the isDebug state.
+   * 
+   * @return isDebug
+   */
+  public static boolean getIsDebug() {
+    return isDebug;
+  }
+
+  /**
+   * Flips the isDebug state to the opposite state of the boolean.
+   */
+  public static void flipIsDebug() {
+    isDebug = !isDebug;
+  }
 
 }
