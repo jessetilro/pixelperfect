3555f58fca4be2bfd924507e4f42a20007857405
David Alderliesten <david.j.w@hotmail.com>
2016-06-01 13:24:46 +0200

    Remove Static Warning
    Remived the PMD and checkstyle warnings from the release branch.

---
+++ src/main/java/nl/tudelft/pixelperfect/game/Settings.java
@@ -0,0 +1,11 @@
+package nl.tudelft.pixelperfect.game;
+
+/**
+ * A singleton data class for settings tracking that are used accross the game.
+ * 
+ * @author David Alderliesten
+ *
+ */
+public final class Settings {
+
+}
