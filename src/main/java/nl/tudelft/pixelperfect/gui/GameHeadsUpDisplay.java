3cc45b8119f5201e9357308bd1e9a0b21c3a407c
David Alderliesten <david.j.w@hotmail.com>
2016-06-02 12:55:11 +0200

    Create HUD class
    Created the HUd class for the display and functionality of the in-game
    hud.

---
+++ src/main/java/nl/tudelft/pixelperfect/gui/GameHeadsUpDisplay.java
@@ -0,0 +1,12 @@
+package nl.tudelft.pixelperfect.gui;
+
+/**
+ * Class responsible for the display, refreshing, and functionality of the heads-up display (HUD)
+ * that is displayed during the active gameplay for the Client.
+ * 
+ * @author David Alderliesten
+ *
+ */
+public class GameHeadsUpDisplay {
+
+}
