2b91c1c6ef920db561a7a81363e2c72e8f1d0536
Floris Doolaard <floris.do@gmail.com>
2016-06-16 19:51:13 +0200

    Removed anything related with routeNodes.
    Also the first commit I tried to enlist some values with an actionlistener, this does not work.

--- src/test/java/nl/tudelft/pixelperfect/SpaceshipTest.java
+++ src/test/java/nl/tudelft/pixelperfect/SpaceshipTest.java
@@ -19,9 +19,9 @@ import com.jme3.network.HostedConnection;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import nl.tudelft.pixelperfect.event.EventListener;
 import nl.tudelft.pixelperfect.event.EventLog;
+import nl.tudelft.pixelperfect.game.Route;
 import nl.tudelft.pixelperfect.game.Spaceship;
 import nl.tudelft.pixelperfect.player.Player;
-import nl.tudelft.pixelperfect.route.Route;
 
 /**
  * Test Suite for the Spaceship class.
