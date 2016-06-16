acc3c20f4a22ef300438cd638f30dc4515d6407c
Floris Doolaard <floris.do@gmail.com>
2016-06-16 19:53:40 +0200

    Moved audio final values to the Constants class.

--- src/main/java/nl/tudelft/pixelperfect/game/Game.java
+++ src/main/java/nl/tudelft/pixelperfect/game/Game.java
@@ -29,9 +29,8 @@ import nl.tudelft.pixelperfect.gui.DebugHeadsUpDisplay;
 import nl.tudelft.pixelperfect.gui.GameHeadsUpDisplay;
 
 /**
- * Main class representing an active Game process and creating the JMonkey
- * Environment. Suppressing the too many fields PMD warning because we will
- * refactor this class next iteration.
+ * Main class representing an active Game process and creating the JMonkey Environment. Suppressing
+ * the too many fields PMD warning because we will refactor this class next iteration.
  * 
  * @author David Alderliesten
  * @author Floris Doolaard
@@ -42,361 +41,362 @@ import nl.tudelft.pixelperfect.gui.GameHeadsUpDisplay;
 @SuppressWarnings("PMD.TooManyFields")
 public class Game extends VRApplication {
 
-	private static Game appGame;
-	private Spaceship spaceship;
-	private EventScheduler scheduler;
-	private static Server server;
-	private AudioPlayer audioPlayer;
-	private Spatial observer;
+  private static Game appGame;
+  private Spaceship spaceship;
+  private EventScheduler scheduler;
+  private static Server server;
+  private AudioPlayer audioPlayer;
+  private Spatial observer;
 
-	private boolean moveForward;
-	private boolean moveBackwards;
-	private boolean rotateLeft;
-	private boolean rotateRight;
-	private boolean startKey;
-	private boolean debugKeyOn;
-	private boolean debugKeyOff;
+  private boolean moveForward;
+  private boolean moveBackwards;
+  private boolean rotateLeft;
+  private boolean rotateRight;
+  private boolean startKey;
+  private boolean debugKeyOn;
+  private boolean debugKeyOff;
 
-	private Scene scene;
+  private Scene scene;
 
-	private DebugHeadsUpDisplay debugHud;
-	private GameHeadsUpDisplay gameHud;
+  private DebugHeadsUpDisplay debugHud;
+  private GameHeadsUpDisplay gameHud;
 
-	private GameState gameState;
+  private GameState gameState;
 
-	/**
-	 * Main method bootstrapping the process by constructing this class and
-	 * initializing a jMonkeyEngine Game.
-	 *
-	 * @param args
-	 *            The parameters passed to the process.
-	 */
-	public static void main(String[] args) {
-		appGame = new Game();
+  /**
+   * Main method bootstrapping the process by constructing this class and initializing a
+   * jMonkeyEngine Game.
+   *
+   * @param args
+   *          The parameters passed to the process.
+   */
+  public static void main(String[] args) {
+    appGame = new Game();
 
-		// Use full screen distortion and maximum FOV.
-		appGame.preconfigureVRApp(PRECONFIG_PARAMETER.USE_CUSTOM_DISTORTION, false);
+    // Use full screen distortion and maximum FOV.
+    appGame.preconfigureVRApp(PRECONFIG_PARAMETER.USE_CUSTOM_DISTORTION, false);
 
-		// Runs faster when set to false, but will allow mirroring.
-		appGame.preconfigureVRApp(PRECONFIG_PARAMETER.ENABLE_MIRROR_WINDOW, true);
+    // Runs faster when set to false, but will allow mirroring.
+    appGame.preconfigureVRApp(PRECONFIG_PARAMETER.ENABLE_MIRROR_WINDOW, true);
 
-		// Render two eyes, regardless of SteamVR.
-		appGame.preconfigureVRApp(PRECONFIG_PARAMETER.FORCE_VR_MODE, false);
-		appGame.preconfigureVRApp(PRECONFIG_PARAMETER.SET_GUI_CURVED_SURFACE, false);
-		appGame.preconfigureVRApp(PRECONFIG_PARAMETER.FLIP_EYES, false);
+    // Render two eyes, regardless of SteamVR.
+    appGame.preconfigureVRApp(PRECONFIG_PARAMETER.FORCE_VR_MODE, false);
+    appGame.preconfigureVRApp(PRECONFIG_PARAMETER.SET_GUI_CURVED_SURFACE, false);
+    appGame.preconfigureVRApp(PRECONFIG_PARAMETER.FLIP_EYES, false);
 
-		// Show gui even if it is behind the current timing.
-		appGame.preconfigureVRApp(PRECONFIG_PARAMETER.SET_GUI_OVERDRAW, true);
+    // Show gui even if it is behind the current timing.
+    appGame.preconfigureVRApp(PRECONFIG_PARAMETER.SET_GUI_OVERDRAW, true);
 
-		// Faster VR rendering, requires some vertex shader changes.
-		appGame.preconfigureVRApp(PRECONFIG_PARAMETER.INSTANCE_VR_RENDERING, false);
-		appGame.preconfigureVRApp(PRECONFIG_PARAMETER.NO_GUI, false);
+    // Faster VR rendering, requires some vertex shader changes.
+    appGame.preconfigureVRApp(PRECONFIG_PARAMETER.INSTANCE_VR_RENDERING, false);
+    appGame.preconfigureVRApp(PRECONFIG_PARAMETER.NO_GUI, false);
 
-		// Set frustum distances here before app starts.
-		// appGame.preconfigureFrustrumNearFar(0.1f, 512f);
+    // Set frustum distances here before app starts.
+    // appGame.preconfigureFrustrumNearFar(0.1f, 512f);
 
-		appGame.start();
-	}
+    appGame.start();
+  }
 
-	/**
-	 * Method initializing the game (e.g. setting up the scenegraph and/or the
-	 * main menu).
-	 */
-	@Override
-	public void simpleInitApp() {
-		observer = new Node("observer");
-		observer.setLocalTranslation(new Vector3f(0.0f, 2.0f, 0.0f));
-		VRApplication.setObserver(observer);
-		rootNode.attachChild(observer);
+  /**
+   * Method initializing the game (e.g. setting up the scenegraph and/or the main menu).
+   */
+  @Override
+  public void simpleInitApp() {
+    observer = new Node("observer");
+    observer.setLocalTranslation(new Vector3f(0.0f, 2.0f, 0.0f));
+    VRApplication.setObserver(observer);
+    rootNode.attachChild(observer);
 
-		initInputs();
+    initInputs();
 
-		scene = new Scene(this);
-		scene.createMap();
-		audioPlayer = new AudioPlayer(this);
-		String[] names = { "CoffeeEvent", "AsteroidEvent", "FireEvent", "HostileEvent", "PlasmaEvent",
-				"CompleteAsteroidEvent", "CompleteCoffeeEvent", "CompleteFireEvent", "CompleteHostileEvent",
-				"CompletePlasmaEvent" };
-		String[] locations = { "yawn.wav", "impact.wav", "fire_alarm.wav", "sonar_x.wav", "bubbling1.wav",
-				"cannon_x.wav", "drink.wav", "fire_ext.wav", "explosion_x.wav", "hammer.wav" };
-		audioPlayer.loadSounds(names, locations);
+    scene = new Scene(this);
+    scene.createMap();
+    audioPlayer = new AudioPlayer(this);
+    audioPlayer.loadSounds(Constants.AUDIO_EVENTS, Constants.AUDIO_PATH_NAMES);
 
-		initNetwork();
-		spaceship = new Spaceship();
-		scheduler = new EventScheduler(Constants.EVENT_SCHEDULER_INTENSITY_MIN,
-				Constants.EVENT_SCHEDULER_INTENSITY_MAX);
-		scheduler.subscribe(spaceship.getLog());
-		scheduler.start();
-		debugHud = new DebugHeadsUpDisplay(getAssetManager(), guiNode, Constants.DEBUG_ELEMENTS_WIDTH_OFFSET,
-				VRGuiManager.getCanvasSize().getY(), spaceship);
-		gameHud = new GameHeadsUpDisplay(getAssetManager(), guiNode, VRGuiManager.getCanvasSize().getX(),
-				VRGuiManager.getCanvasSize().getY(), spaceship);
+    initNetwork();
+    spaceship = new Spaceship();
+    scheduler = new EventScheduler(Constants.EVENT_SCHEDULER_INTENSITY_MIN,
+        Constants.EVENT_SCHEDULER_INTENSITY_MAX);
+    scheduler.subscribe(spaceship.getLog());
+    scheduler.start();
+    debugHud = new DebugHeadsUpDisplay(getAssetManager(), guiNode,
+        Constants.DEBUG_ELEMENTS_WIDTH_OFFSET, VRGuiManager.getCanvasSize().getY(), spaceship);
+    gameHud = new GameHeadsUpDisplay(getAssetManager(), guiNode, VRGuiManager.getCanvasSize()
+        .getX(), VRGuiManager.getCanvasSize().getY(), spaceship);
 
-		gameState = new StartState(this);
-	}
+    gameState = new StartState(this);
+  }
 
-	/**
-	 * Method to initialize the network.
-	 */
-	private void initNetwork() {
-		try {
-			server = Network.createServer(6143);
-			Serializer.registerClass(EventCompletedMessage.class);
-			Serializer.registerClass(RoleChosenMessage.class);
-			Serializer.registerClass(RepairMessage.class);
-			Serializer.registerClass(NewGameMessage.class);
-			server.start();
-			ServerListener listen = new ServerListener();
-			listen.setGame(this);
-			listen.setServer(server);
-			server.addMessageListener(listen, EventCompletedMessage.class);
-			server.addMessageListener(listen, RoleChosenMessage.class);
-			server.addMessageListener(listen, RepairMessage.class);
-			server.addMessageListener(listen, NewGameMessage.class);
-			ConnectListener connect = new ConnectListener();
-			connect.setGame(this);
-			server.addConnectionListener(connect);
-		} catch (IOException except) {
-			except.printStackTrace();
-		}
-	}
+  /**
+   * Method to initialize the network.
+   */
+  private void initNetwork() {
+    try {
+      server = Network.createServer(6143);
+      Serializer.registerClass(EventCompletedMessage.class);
+      Serializer.registerClass(RoleChosenMessage.class);
+      Serializer.registerClass(RepairMessage.class);
+      Serializer.registerClass(NewGameMessage.class);
+      server.start();
+      ServerListener listen = new ServerListener();
+      listen.setGame(this);
+      listen.setServer(server);
+      server.addMessageListener(listen, EventCompletedMessage.class);
+      server.addMessageListener(listen, RoleChosenMessage.class);
+      server.addMessageListener(listen, RepairMessage.class);
+      server.addMessageListener(listen, NewGameMessage.class);
+      ConnectListener connect = new ConnectListener();
+      connect.setGame(this);
+      server.addConnectionListener(connect);
+    } catch (IOException except) {
+      except.printStackTrace();
+    }
+  }
 
-	/**
-	 * Tells the clients to start the game.
-	 */
-	public static void startGame() {
-		server.broadcast(new NewGameMessage());
-	}
+  /**
+   * Tells the clients to start the game.
+   */
+  public static void startGame() {
+    server.broadcast(new NewGameMessage());
+  }
 
-	/**
-	 * Initiate input for the game.
-	 */
-	@SuppressWarnings({ "checkstyle:methodlength", "PMD" })
-	private void initInputs() {
-		InputManager inputManager = getInputManager();
-		int[] keyTriggers = { KeyInput.KEY_W, KeyInput.KEY_S, KeyInput.KEY_A, KeyInput.KEY_D, KeyInput.KEY_P,
-				KeyInput.KEY_0, KeyInput.KEY_1 };
-    final String[] mappings = { "forward", "back", "left", "right", "start", "debugOn", "debugOff" };
-		for (int i = 0; i < keyTriggers.length; i++) {
-			inputManager.addMapping(mappings[i], new KeyTrigger(keyTriggers[i]));
-		}
-    final Boolean[] moves = { moveForward, moveBackwards, rotateLeft, rotateRight, startKey,
-        debugKeyOn, debugKeyOff };
-		ActionListener acl = new ActionListener() {
-
-			public void onAction(String name, boolean keyPressed, float tpf) {
-        for (int j = 0; j < moves.length; j++) {
-          if (name.equals(mappings[j])) {
-            moves[j] = keyPressed;
-          }
+  /**
+   * Initiate input for the game.
+   */
+  @SuppressWarnings({ "checkstyle:methodlength", "PMD" })
+  private void initInputs() {
+    InputManager inputManager = getInputManager();
+    int[] keyTriggers = { KeyInput.KEY_W, KeyInput.KEY_S, KeyInput.KEY_A, KeyInput.KEY_D,
+        KeyInput.KEY_P, KeyInput.KEY_0, KeyInput.KEY_1 };
+    String[] mappings = { "forward", "back", "left", "right", "start", "debugOn", "debugOff" };
+    for (int i = 0; i < keyTriggers.length; i++) {
+      inputManager.addMapping(mappings[i], new KeyTrigger(keyTriggers[i]));
+    }
+    ActionListener acl = new ActionListener() {
+      public void onAction(String name, boolean keyPressed, float tpf) {
+        if (name.equals("forward")) {
+          moveForward = keyPressed;
+        } else if (name.equals("back")) {
+          moveBackwards = keyPressed;
+        } else if (name.equals("left")) {
+          rotateLeft = keyPressed;
+        } else if (name.equals("right")) {
+          rotateRight = keyPressed;
+        } else if (name.equals("start")) {
+          startKey = keyPressed;
+        } else if (name.equals("debugOn")) {
+          debugKeyOn = keyPressed;
+        } else if (name.equals("debugOff")) {
+          debugKeyOff = keyPressed;
         }
-			}
-		};
-		for (String mapping : mappings) {
-			inputManager.addListener(acl, mapping);
-		}
-	}
+      }
+    };
+    for (String mapping : mappings) {
+      inputManager.addListener(acl, mapping);
+    }
+  }
 
-	/**
-	 * Get the spaceship for reference purposes.
-	 *
-	 * @return The spaceship.
-	 */
-	public Spaceship getSpaceship() {
-		return spaceship;
-	}
+  /**
+   * Get the spaceship for reference purposes.
+   *
+   * @return The spaceship.
+   */
+  public Spaceship getSpaceship() {
+    return spaceship;
+  }
 
-	/**
-	 * Main update loop for the game.
-	 */
-	@Override
-	public void simpleUpdate(float tpf) {
-		gameState.update(tpf);
-		gameState = gameState.handleState();
-	}
+  /**
+   * Main update loop for the game.
+   */
+  @Override
+  public void simpleUpdate(float tpf) {
+    gameState.update(tpf);
+    gameState = gameState.handleState();
+  }
 
-	/**
-	 * Getter for observer.
-	 *
-	 * @return observer
-	 */
-	public Spatial getGameObserver() {
-		return observer;
-	}
+  /**
+   * Getter for observer.
+   *
+   * @return observer
+   */
+  public Spatial getGameObserver() {
+    return observer;
+  }
 
-	/**
-	 * Getter for moveForward.
-	 *
-	 * @return moveForward
-	 */
-	public boolean isMoveForward() {
-		return moveForward;
-	}
+  /**
+   * Getter for moveForward.
+   *
+   * @return moveForward
+   */
+  public boolean isMoveForward() {
+    return moveForward;
+  }
 
-	/**
-	 * Getter for moveBackwards.
-	 *
-	 * @return moveBackwards
-	 */
-	public boolean isMoveBackwards() {
-		return moveBackwards;
-	}
+  /**
+   * Getter for moveBackwards.
+   *
+   * @return moveBackwards
+   */
+  public boolean isMoveBackwards() {
+    return moveBackwards;
+  }
 
-	/**
-	 * Getter for rotateLeft.
-	 *
-	 * @return rotateLeft
-	 */
-	public boolean isRotateLeft() {
-		return rotateLeft;
-	}
+  /**
+   * Getter for rotateLeft.
+   *
+   * @return rotateLeft
+   */
+  public boolean isRotateLeft() {
+    return rotateLeft;
+  }
 
-	/**
-	 * Getter for rotateRight.
-	 *
-	 * @return rotateRight
-	 */
-	public boolean isRotateRight() {
-		return rotateRight;
-	}
+  /**
+   * Getter for rotateRight.
+   *
+   * @return rotateRight
+   */
+  public boolean isRotateRight() {
+    return rotateRight;
+  }
 
-	/**
-	 * Getter for startKey.
-	 *
-	 * @return startKey
-	 */
-	public boolean isStartKey() {
-		return startKey;
-	}
+  /**
+   * Getter for startKey.
+   *
+   * @return startKey
+   */
+  public boolean isStartKey() {
+    return startKey;
+  }
 
-	/**
-	 * Getter for the debugKey activator.
-	 *
-	 * @return debugKeyOn
-	 */
-	public boolean isDebugOnTrigger() {
-		return debugKeyOn;
-	}
+  /**
+   * Getter for the debugKey activator.
+   *
+   * @return debugKeyOn
+   */
+  public boolean isDebugOnTrigger() {
+    return debugKeyOn;
+  }
 
-	/**
-	 * Getter for the debugKey deactivator.
-	 *
-	 * @return debugKeyOff
-	 */
-	public boolean isDebugOffTrigger() {
-		return debugKeyOff;
-	}
+  /**
+   * Getter for the debugKey deactivator.
+   *
+   * @return debugKeyOff
+   */
+  public boolean isDebugOffTrigger() {
+    return debugKeyOff;
+  }
 
-	/**
-	 * Getter for the debugHud.
-	 *
-	 * @return debugHud
-	 */
-	public DebugHeadsUpDisplay getDebugHud() {
-		return debugHud;
-	}
+  /**
+   * Getter for the debugHud.
+   *
+   * @return debugHud
+   */
+  public DebugHeadsUpDisplay getDebugHud() {
+    return debugHud;
+  }
 
-	/**
-	 * Getter for the gameHud.
-	 *
-	 * @return gameHud
-	 */
-	public GameHeadsUpDisplay getGameHud() {
-		return gameHud;
-	}
+  /**
+   * Getter for the gameHud.
+   *
+   * @return gameHud
+   */
+  public GameHeadsUpDisplay getGameHud() {
+    return gameHud;
+  }
 
-	/**
-	 * Getter for scheduler.
-	 *
-	 * @return scheduler
-	 */
-	public EventScheduler getScheduler() {
-		return scheduler;
-	}
+  /**
+   * Getter for scheduler.
+   *
+   * @return scheduler
+   */
+  public EventScheduler getScheduler() {
+    return scheduler;
+  }
 
-	/**
-	 * Setter for debugDisplay.
-	 *
-	 * @param debugDisplay
-	 *            debugDisplay to be set.
-	 */
-	public void setDebugDisplay(DebugHeadsUpDisplay debugDisplay) {
-		this.debugHud = debugDisplay;
-	}
+  /**
+   * Setter for debugDisplay.
+   *
+   * @param debugDisplay
+   *          debugDisplay to be set.
+   */
+  public void setDebugDisplay(DebugHeadsUpDisplay debugDisplay) {
+    this.debugHud = debugDisplay;
+  }
 
-	/**
-	 * Setter for the gameDisplay.
-	 *
-	 * @param passedDisplay
-	 *            gameDisplay to be set.
-	 */
-	public void setHeadsUpDisplay(GameHeadsUpDisplay passedDisplay) {
-		this.gameHud = passedDisplay;
-	}
+  /**
+   * Setter for the gameDisplay.
+   *
+   * @param passedDisplay
+   *          gameDisplay to be set.
+   */
+  public void setHeadsUpDisplay(GameHeadsUpDisplay passedDisplay) {
+    this.gameHud = passedDisplay;
+  }
 
-	/**
-	 * Setter for observer.
-	 *
-	 * @param observer
-	 *            Observer to be set.
-	 */
-	public void setGameObserver(Spatial observer) {
-		this.observer = observer;
-	}
+  /**
+   * Setter for observer.
+   *
+   * @param observer
+   *          Observer to be set.
+   */
+  public void setGameObserver(Spatial observer) {
+    this.observer = observer;
+  }
 
-	/**
-	 * Setter for scheduler.
-	 *
-	 * @param scheduler
-	 *            Scheduler to be set.
-	 */
-	public void setScheduler(EventScheduler scheduler) {
-		this.scheduler = scheduler;
-	}
+  /**
+   * Setter for scheduler.
+   *
+   * @param scheduler
+   *          Scheduler to be set.
+   */
+  public void setScheduler(EventScheduler scheduler) {
+    this.scheduler = scheduler;
+  }
 
-	/**
-	 * Setter for spaceship.
-	 *
-	 * @param spaceship
-	 *            Spaceship to be set.
-	 */
-	public void setSpaceship(Spaceship spaceship) {
-		this.spaceship = spaceship;
-	}
+  /**
+   * Setter for spaceship.
+   *
+   * @param spaceship
+   *          Spaceship to be set.
+   */
+  public void setSpaceship(Spaceship spaceship) {
+    this.spaceship = spaceship;
+  }
 
-	/**
-	 * Getter for the audio player.
-	 *
-	 * @return audio player
-	 */
-	public AudioPlayer getAudioPlayer() {
-		return audioPlayer;
-	}
+  /**
+   * Getter for the audio player.
+   *
+   * @return audio player
+   */
+  public AudioPlayer getAudioPlayer() {
+    return audioPlayer;
+  }
 
-	/**
-	 * Getter for the scene.
-	 *
-	 * @return scene
-	 */
-	public Scene getScene() {
-		return scene;
-	}
+  /**
+   * Getter for the scene.
+   *
+   * @return scene
+   */
+  public Scene getScene() {
+    return scene;
+  }
 
-	/**
-	 * Returns the size of the x viewport for VR.
-	 * 
-	 * @return viewport width.
-	 */
-	public float getViewPortX() {
-		return VRGuiManager.getCanvasSize().getX();
-	}
+  /**
+   * Returns the size of the x viewport for VR.
+   * 
+   * @return viewport width.
+   */
+  public float getViewPortX() {
+    return VRGuiManager.getCanvasSize().getX();
+  }
 
-	/**
-	 * Returns the size of the y viewport for VR.
-	 * 
-	 * @return viewport height.
-	 */
-	public float getViewPortY() {
-		return VRGuiManager.getCanvasSize().getY();
-	}
+  /**
+   * Returns the size of the y viewport for VR.
+   * 
+   * @return viewport height.
+   */
+  public float getViewPortY() {
+    return VRGuiManager.getCanvasSize().getY();
+  }
 }
