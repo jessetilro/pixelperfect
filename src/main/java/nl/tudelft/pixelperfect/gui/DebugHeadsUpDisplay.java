a9ee48294443c34511a0822c0da2c197e89efd4e
David Alderliesten <david.j.w@hotmail.com>
2016-06-16 11:36:01 +0200

    Implement newlines debug
    Added the newline option to the debug view in VR to show events when
    active.

--- src/main/java/nl/tudelft/pixelperfect/gui/DebugHeadsUpDisplay.java
+++ src/main/java/nl/tudelft/pixelperfect/gui/DebugHeadsUpDisplay.java
@@ -13,150 +13,156 @@ import java.net.InetAddress;
 import java.util.ArrayList;
 
 /**
- * Class that displays relevant information in the heads-up display during the gameplay when the
- * debug mode has been activated.
+ * Class that displays relevant information in the heads-up display during the
+ * gameplay when the debug mode has been activated.
  * 
  * @author David Alderliesten
  *
  */
 public class DebugHeadsUpDisplay {
-  private Spaceship spaceship;
+	private Spaceship spaceship;
 
-  private AssetManager assetManager;
-  private Node guiNodes;
-  private float screenWidth;
-  private float screenHeight;
+	private AssetManager assetManager;
+	private Node guiNodes;
+	private float screenWidth;
+	private float screenHeight;
 
-  private BitmapFont hudFont;
-  private BitmapText captainLog;
-  private BitmapText shipHealth;
-  private BitmapText teamScore;
-  private BitmapText timeLeft;
-  private BitmapText playersConnected;
-  private BitmapText localAddress;
+	private BitmapFont hudFont;
+	private BitmapText captainLog;
+	private BitmapText shipHealth;
+	private BitmapText teamScore;
+	private BitmapText timeLeft;
+	private BitmapText playersConnected;
+	private BitmapText localAddress;
 
-  /**
-   * Constructor for the debug heads-up display for in-game utilization.
-   * 
-   * @param passedMan
-   *          the passed assetmanager.
-   * @param passedGuiNode
-   *          the passed gui node.
-   * @param width
-   *          the passed screen width.
-   * @param height
-   *          the passed screen height.
-   * @param passedShip
-   *          the passed Spaceship instance.
-   */
-  public DebugHeadsUpDisplay(AssetManager passedMan, Node passedGuiNode, float width, float height,
-      Spaceship passedShip) {
-    this.assetManager = passedMan;
-    this.guiNodes = passedGuiNode;
-    this.screenWidth = width;
-    this.screenHeight = height;
-    this.spaceship = passedShip;
+	/**
+	 * Constructor for the debug heads-up display for in-game utilization.
+	 * 
+	 * @param passedMan
+	 *            the passed assetmanager.
+	 * @param passedGuiNode
+	 *            the passed gui node.
+	 * @param width
+	 *            the passed screen width.
+	 * @param height
+	 *            the passed screen height.
+	 * @param passedShip
+	 *            the passed Spaceship instance.
+	 */
+	public DebugHeadsUpDisplay(AssetManager passedMan, Node passedGuiNode, float width, float height,
+			Spaceship passedShip) {
+		this.assetManager = passedMan;
+		this.guiNodes = passedGuiNode;
+		this.screenWidth = width;
+		this.screenHeight = height;
+		this.spaceship = passedShip;
 
-    // Sets-up the text display using the private method.
-    setupTextDisplay();
-  }
+		// Sets-up the text display using the private method.
+		setupTextDisplay();
+	}
 
-  /**
-   * Sets-up text display for the in-game debug HUD, along with all of its elements.
-   */
-  private void setupTextDisplay() {
-    // Set-up the bitmap text needed for HUD display.
-    setUpBitmapText();
+	/**
+	 * Sets-up text display for the in-game debug HUD, along with all of its
+	 * elements.
+	 */
+	private void setupTextDisplay() {
+		// Set-up the bitmap text needed for HUD display.
+		setUpBitmapText();
 
-    // Add the generated bitmaps to the gui node view.
-    guiNodes.attachChild(captainLog);
-    guiNodes.attachChild(shipHealth);
-    guiNodes.attachChild(teamScore);
-    guiNodes.attachChild(timeLeft);
-    guiNodes.attachChild(playersConnected);
-    guiNodes.attachChild(localAddress);
-  }
+		// Add the generated bitmaps to the gui node view.
+		guiNodes.attachChild(captainLog);
+		guiNodes.attachChild(shipHealth);
+		guiNodes.attachChild(teamScore);
+		guiNodes.attachChild(timeLeft);
+		guiNodes.attachChild(playersConnected);
+		guiNodes.attachChild(localAddress);
+	}
 
-  /**
-   * Set-up the bitmap associations for the debug HUD text.
-   */
-  private void setUpBitmapText() {
-    // Loading the font stored in the jme default manager.
-    hudFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+	/**
+	 * Set-up the bitmap associations for the debug HUD text.
+	 */
+	private void setUpBitmapText() {
+		// Loading the font stored in the jme default manager.
+		hudFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
 
-    // Init for the log text, including font loading and text setting.
-    captainLog = new BitmapText(hudFont, true);
-    captainLog.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_LOG_HEIGHT_OFFSET,
-        0);
+		// Init for the log text, including font loading and text setting.
+		captainLog = new BitmapText(hudFont, true);
+		captainLog.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_LOG_HEIGHT_OFFSET, 0);
 
-    // Init for the HUD text, including font loading and text setting.
-    shipHealth = new BitmapText(hudFont, true);
-    shipHealth.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_HEALTH_HEIGHT_OFFSET,
-        0);
+		// Init for the HUD text, including font loading and text setting.
+		shipHealth = new BitmapText(hudFont, true);
+		shipHealth.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_HEALTH_HEIGHT_OFFSET, 0);
 
-    teamScore = new BitmapText(hudFont, true);
-    teamScore.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_SCORE_HEIGHT_OFFSET,
-        0);
+		teamScore = new BitmapText(hudFont, true);
+		teamScore.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_SCORE_HEIGHT_OFFSET, 0);
 
-    timeLeft = new BitmapText(hudFont, true);
-    timeLeft.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_TIME_HEIGHT_OFFSET, 0);
+		timeLeft = new BitmapText(hudFont, true);
+		timeLeft.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_TIME_HEIGHT_OFFSET, 0);
 
-    playersConnected = new BitmapText(hudFont, true);
-    playersConnected.setLocalTranslation(screenWidth,
-        screenHeight - Constants.DEBUG_CONNECTED_HEIGHT_OFFSET, 0);
+		playersConnected = new BitmapText(hudFont, true);
+		playersConnected.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_CONNECTED_HEIGHT_OFFSET, 0);
 
-    localAddress = new BitmapText(hudFont, true);
-    localAddress.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_IP_HEIGHT_OFFSET,
-        0);
-  }
+		localAddress = new BitmapText(hudFont, true);
+		localAddress.setLocalTranslation(screenWidth, screenHeight - Constants.DEBUG_IP_HEIGHT_OFFSET, 0);
+	}
 
-  /**
-   * Method responsible for the updating of the debug hud text and displaying the events, along with
-   * their remaining time and health, and other HUD elements.
-   */
-  public void updateHud() {
-    ArrayList<Event> currentEvents = spaceship.getLog().getEvents();
-    ArrayList<String> currentEventsToDisplay = new ArrayList<String>();
+	/**
+	 * Method responsible for the updating of the debug hud text and displaying
+	 * the events, along with their remaining time and health, and other HUD
+	 * elements.
+	 */
+	public void updateHud() {
+		ArrayList<Event> currentEvents = spaceship.getLog().getEvents();
+		ArrayList<String> currentEventsToDisplay = new ArrayList<String>();
 
-    // Fetch all event times for updates and display.
-    for (Event current : currentEvents) {
-      currentEventsToDisplay.add(current.toDebugString());
-    }
+		// Fetch all event times for updates and display.
+		for (Event current : currentEvents) {
+			currentEventsToDisplay.add(current.toDebugString());
+		}
 
-    // Update the captain's log.
-    if (currentEventsToDisplay.isEmpty()) {
-      captainLog.setText(Constants.DEBUG_NO_EVENTS_LOG_TEXT);
-    } else {
-      captainLog.setText(currentEventsToDisplay.toString());
-    }
+		// Update the captain's log.
+		if (currentEventsToDisplay.isEmpty()) {
+			captainLog.setText(Constants.DEBUG_NO_EVENTS_LOG_TEXT);
+		} else {
+			StringBuilder toShow = new StringBuilder();
 
-    // Update the ship's health, team score, connected players, and time left indicators.
-    shipHealth.setText(Constants.DEBUG_SHIP_HEALTH_LABEL + spaceship.getHealth());
-    teamScore.setText(Constants.DEBUG_SHIP_SCORE_LABEL + spaceship.getScore());
-    timeLeft.setText(Constants.DEBUG_SHIP_TIME_LABEL + spaceship.getTimer());
-    playersConnected.setText(Constants.DEBUG_CONNECTED_LABEL + spaceship.getCrew().size());
+			// Adding spacing between the lines.
+			for (String current : currentEventsToDisplay) {
+				toShow.append(current);
+				toShow.append("\n");
+			}
 
-    // Printing the server local IP address.
-    try {
-      localAddress.setText(Constants.DEBUG_IP_LABEL + InetAddress.getLocalHost());
-    } catch (Exception error) {
-      // Printing the IP error if it occurs.
-      System.out.println(error);
-    }
-  }
+			captainLog.setText(toShow.toString());
+		}
 
-  /**
-   * Clears the debug display after de-activation of the debug mode. The strings are only cleared
-   * instead of detached due to the ability to re-start the debug mode, which causes issues if
-   * re-initialized.
-   */
-  public void clearHud() {
-    captainLog.setText("");
-    shipHealth.setText("");
-    teamScore.setText("");
-    timeLeft.setText("");
-    playersConnected.setText("");
-    localAddress.setText("");
-  }
+		// Update the ship's health, team score, connected players, and time
+		// left indicators.
+		shipHealth.setText(Constants.DEBUG_SHIP_HEALTH_LABEL + spaceship.getHealth());
+		teamScore.setText(Constants.DEBUG_SHIP_SCORE_LABEL + spaceship.getScore());
+		timeLeft.setText(Constants.DEBUG_SHIP_TIME_LABEL + spaceship.getTimer());
+		playersConnected.setText(Constants.DEBUG_CONNECTED_LABEL + spaceship.getCrew().size());
+
+		// Printing the server local IP address.
+		try {
+			localAddress.setText(Constants.DEBUG_IP_LABEL + InetAddress.getLocalHost());
+		} catch (Exception error) {
+			// Printing the IP error if it occurs.
+			System.out.println(error);
+		}
+	}
+
+	/**
+	 * Clears the debug display after de-activation of the debug mode. The
+	 * strings are only cleared instead of detached due to the ability to
+	 * re-start the debug mode, which causes issues if re-initialized.
+	 */
+	public void clearHud() {
+		captainLog.setText("");
+		shipHealth.setText("");
+		teamScore.setText("");
+		timeLeft.setText("");
+		playersConnected.setText("");
+		localAddress.setText("");
+	}
 
 }
