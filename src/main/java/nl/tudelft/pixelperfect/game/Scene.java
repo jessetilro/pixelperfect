4b7289b7e35acafca3007cc7298ec7f0922ba0d4
wouterz <zirkzee@hotmail.com>
2016-06-17 16:48:41 +0200

    remove unused and old code

--- src/main/java/nl/tudelft/pixelperfect/game/Scene.java
+++ src/main/java/nl/tudelft/pixelperfect/game/Scene.java
@@ -4,15 +4,11 @@ import com.jme3.font.BitmapFont;
 import com.jme3.font.BitmapText;
 import com.jme3.light.Light;
 import com.jme3.light.PointLight;
-import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
-import com.jme3.texture.Texture;
 
 /**
  * Class for drawing objects in the game.
@@ -22,9 +18,7 @@ import com.jme3.texture.Texture;
  */
 public class Scene {
 
-  private static final Vector3f XAXIS = new Vector3f(1, 0, 0);
   private static final Vector3f YAXIS = new Vector3f(0, 1, 0);
-//  private static final Vector3f ZAXIS = new Vector3f(0, 0, 1);
 
   /**
    * Get the light object.
@@ -37,7 +31,6 @@ public class Scene {
   private Light light = new PointLight(new Vector3f(0, 1, 0));
 
   private Game app;
-  private String basicMat;
   private BitmapFont font;
   private BitmapText hostileEventText;
   private BitmapText asteroidEventLabel;
@@ -78,7 +71,6 @@ public class Scene {
    */
   public Scene(Game game) {
     app = game;
-    basicMat = "Common/MatDefs/Misc/Unshaded.j3md";
     font = app.getAssetManager().loadFont("Interface/Fonts/Default.fnt");
   }
 
@@ -86,18 +78,14 @@ public class Scene {
    * Method that contains all objects for the scene.
    */
   public void createMap() {
-    asteroidEventLabel = createLabel(new Vector3f(7, 0.65f, -3.5f), "ASTEROID WARNING",
-        .2f, ColorRGBA.Blue, new Quaternion().fromAngleAxis(-FastMath.HALF_PI, YAXIS));
-    fireEventLabel = createLabel(new Vector3f(-7, 0.6f, -1f), "FIRE WARNING",
+    asteroidEventLabel = createLabel(new Vector3f(7, 0.65f, -4f), "ASTEROID WARNING",
+        .2f, ColorRGBA.Blue, new Quaternion().fromAngleAxis(-FastMath.HALF_PI * .9f, YAXIS));
+    fireEventLabel = createLabel(new Vector3f(-7, 0.7f, -0.5f), "FIRE WARNING",
         .2f, ColorRGBA.Red, new Quaternion().fromAngleAxis(FastMath.HALF_PI, YAXIS));
     plasmaEventLabel = createLabel(new Vector3f(-9, 2.5f, -19), "PLASMA LEAK",
         .5f, ColorRGBA.Green, new Quaternion());
     hostileEventText = createLabel(new Vector3f(1, 4f, 19f), "x: \ny: \n",
         .5f, ColorRGBA.Green, new Quaternion().fromAngleAxis(FastMath.PI, new Vector3f(0, 1, 0)));
-//    Geometry radar = createBoxObject(new Box(0.6f, 0.4f, 0.01f),
-//        new Vector3f(-1.3f, 0.4f, 12.5f), "Textures/radar.jpg");
-//    radar.setLocalRotation(new Quaternion().fromAngleAxis(FastMath.PI / 4, XAXIS));
-
 
     Spatial spaceship = app.getAssetManager().loadModel("Models/spaceship/spaceship_no_light.j3o");
     spaceship.scale(2f);
@@ -108,30 +96,6 @@ public class Scene {
     pipes.setLocalTranslation(-8, 5, -22);
     pipes.setLocalRotation(new Quaternion().fromAngleAxis(FastMath.PI * 0.3f, YAXIS));
     app.getRootNode().attachChild(pipes);
-  }
-
-  /**
-   * Create a box object.
-   *
-   * @param box
-   *          New box with the dimensions of the box.
-   * @param translation
-   *          Translation of the box.
-   * @param textureLocation
-   *          Texture of the box.
-   */
-  private Geometry createBoxObject(Box box, Vector3f translation, String textureLocation) {
-    Box newBox = box;
-    Geometry geometry = new Geometry("Box", newBox);
-    Material material = new Material(app.getAssetManager(), basicMat);
-    Texture texture = app.getAssetManager().loadTexture(textureLocation);
-    material.setTexture("ColorMap", texture);
-    geometry.setMaterial(material);
-    geometry.setLocalTranslation(translation);
-
-    app.getRootNode().attachChild(geometry);
-
-    return geometry;
   }
 
   /**
